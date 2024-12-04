4aa973923741df783ee11e6f4df646fe882c28f3
miriceomni <miriceomni@gmail.com>
2024-12-04 11:47:36 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,158 @@
+# nucleus-private-projects
+
+
+
+## Description
+The project-tool tool was created to demo a possible way to organize a folder structure that limits what normal 'users' can see and access. This type of behaviour is somewhat a mimic of the /Users folder structure. In that case all non-admin users can see the folder the folder '/Users' but in that folder they can only see the sub-folder for their user-id.
+
+This tool can be used to create/manage a new folder structure that can be used to hide sub-folders from all users that are not part of that folders ACL group.
+
+This tool was created to do the folllowing 
+- create a base 'Projects' folder at the root of Nucleus. In this architecture, all users can see this folder in Omniverse applicatons, Navigator iagor or Nucleus web page on Launcher.
+
+
+## Prereqs
+
+### Clone this repo
+The following documetation is based on creating a directory called ***omniverse*** somewhere on the client **local** filesystem. The name of this directory is up to the user.
+
+
+
+
+<details>
+  <summary>Windows</summary>
+
+```
+PS > cd C:\
+PS > mkdir omniverse
+PS > cd C:\omniverse
+PS > git clone repo ssh://git@gitlab-master.nvidia.com:12051/nves/omniveres/nucleus-private-projects.git
+```
+</details>
+<details>
+  <summary>Linux</summary>
+
+```
+$ cd ~
+$ mkdir omniverse
+$ cd omniverse
+$ git clone repo ssh://git@gitlab-master.nvidia.com:12051/nves/omniveres/nucleus-private-projects.git
+```
+</details>
+
+### Install Omnivese Connect Samples
+Download and build the Omniverse Connect Samples. Site to get Connect SDK https://github.com/NVIDIA-Omniverse/connect-samples/releases.
+Use this site to get a pointer to the current archive
+
+Place this download zip/tar.gz anywhere on the client **local** filesystem.
+> Note: The SDK cannot be installed in a OneDrive location.
+
+<details>
+  <summary>Windows</summary>
+
+```
+PS > cd C:\omniverse\nucleus-private-projects 
+PS > wget https://github.com/NVIDIA-Omniverse/connect-samples/archive/refs/tags/v205.0.0.zip -OutFile connect-sdk.zip
+PS > Expand-Archive .\connect-sdk.zip
+PS > rm connect-sdk.zip
+PS > cd connect-sdk\connect-samples-205.0.0
+PS > ./build.bat
+```
+</details>
+<details>
+  <summary>Linux</summary>
+
+```
+$ cd ~/omniverse/nucleus-private-projects
+$ wget https://github.com/NVIDIA-Omniverse/connect-samples/archive/refs/tags/v205.0.0.tar.gz  -O connect-sdk.tar.gz
+$ mkdir -p connect-sdk && tar -xvf connect-sdk.tar.gz -C connect-sdk/
+$ rm connect-sdk.tar.gz
+$ cd connect-sdk/connect-samples-205.0.0/
+$ ./build.sh
+```
+</details>
+
+
+
+
+## Setup
+Once the Omniverse Connect Samples has been downloaded and built, update the `project-tool.bat` or `project-tool.sh` to define the path to the root of the Omniverse Connect Samples. These scripts have comments to where to make the modifications. Following is snippet from `project-tool.bat`
+
+```
+::
+::  Change this line to point to the downloaded (and built)
+::  Client library root dir. 
+::
+set CLIENT_LIB_SDK_DIR=C:\omniverse\nucleus-private-projects\connect-sdk\connect-samples-205.0.0\
+```
+
+## Usage
+```
+$ ./project-tool.bat  -h
+usage: project-tool.py [-h] [-u USER_ID] [-p PASSWORD] [-M METHOD] [-O OPERATION] [pos_args ...]
+
+Python Client to setup private project infrastructure
+
+positional arguments:
+  pos_args              positional args
+                        1. Nucleus_server (ex: ov-elysium.redshiftltd.net)
+                        2. Project_root (ex: 'NVEX_Projects')
+                        3. Project_name (ex: 'Project_C'
+                        4. User_name (ex: 'mike')
+
+options:
+  -h, --help            show this help message and exit
+  -u USER_ID, --user_id USER_ID
+  -p PASSWORD, --password PASSWORD
+  -M METHOD, --method METHOD
+
+                        Tool method
+                        values 0-3
+                        Mike
+  -O OPERATION, --operation OPERATION
+
+                        operation mode
+                        values 0,1
+                        0=add (default)
+                        1=delete
+```
+
+
+
+
+### Example 
+#### Create a Root Project
+Create a Root Project folder called 'NVEX_projects'
+```
+./project-tool -u -felix -p admin -M 1 -O 0 ov-elysium.redshiftltd.net NVEX_Projects  
+```
+The -M 1 option is used to denote a root project folder operation, the -O 0 means add (use -O 1 to delete). One needs to do this only once for every major root project folder creation. After this is done, one can add any number of sub-projects to this root project folder. 
+
+#### Create a Sub-Project under root
+The following example shows how to add a sub-project under the root project folder called Project_C:
+
+```
+./project-tool -u -felix -p admin -M 2 -O 0 ov-elysium.redshiftltd.net NVEX_Projects Project_C
+```
+
+#### Add user to Sub-project
+```
+./project-tool -u -felix -p admin -M 3 -O 0 ov-elysium.redshiftltd.net NVEX_Projects Project_C user_a   
+```
+
+
+
+## Support
+TBD.
+
+
+## Authors and acknowledgment
+TBD.
+
+## License
+For open source projects, say how it is licensed.
+
+
+
+
+
---
+++ project-tool.py
@@ -0,0 +1,205 @@
+import argparse
+from argparse import RawTextHelpFormatter
+from pathlib import Path
+
+import omni.client
+import time
+
+g_1 = None 
+g_2 = None
+
+g_control_data = {
+    'nucleus_user'      : 'omniverse',
+    'nucleus_password'  : 'RR123456',
+}
+
+g_operation = ['Add','Delete']
+g_methods = ['Nothing','Root Project','Sub Project','User']
+
+
+
+def printf(str,type = 0):
+    types = ['Success','Error','Warn']
+    print(f"[project-tool]: {types[type]} -- {str}")
+
+def authentication_callback(url):
+    print("[project-tool]: Authenticating to {}".format(url))
+    return g_control_data["nucleus_user"], g_control_data["nucleus_password"]
+   
+def connectionStatusCallback(url, connectionStatus):
+    print("[project-tool]: Connection status to {} is {}".format(url, connectionStatus))
+    
+def connect_to_nucleus():
+    global g_1, g_2
+
+    try:
+        if not omni.client.initialize():
+            return "Failed to initialize Omniverse Client"
+
+        print("[project-tool]: Omniverse Client initialized" + omni.client.get_version()) # version 2.17
+
+        g_1 = omni.client.register_authorize_callback(authentication_callback)
+        g_2 = omni.client.register_connection_status_callback(connectionStatusCallback)
+
+    except Exception as e:
+        print("[project-tool]: The error is: ",e)
+    
+def get_nucleus_url():
+    return f"omniverse://{g_control_data['nucleus']}"
+
+def startupOmniverse():
+    connect_to_nucleus()
+    pass
+
+def shutdownOmniverse():
+    omni.client.sign_out(get_nucleus_url())
+    omni.client.shutdown()
+
+
+
+
+    
+# tool -u -p -M 1 -O 0 nucleus_url Project_root   
+# tool -u -p -M 2 -O 0 nucleus_url Project_root  Project_name
+# tool -u -p -M 3 -O 0 nucleus_url Project_root  Project_name  User_name
+ 
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Python Client to setup private project infrastructure', 
+                                     formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument('pos_args', nargs='*', 
+                        help="positional args \n1. Nucleus_server (ex: ov-elysium.redshiftltd.net)\n2. Project_root (ex: 'NVEX_Projects')\n3. Project_name (ex: 'Project_C' \n4. User_name (ex: 'mike')") 
+  
+
+    parser.add_argument("-u", "--user_id",  action='store', default="omniverse")
+    parser.add_argument("-p", "--password", action='store', default="123456")
+    parser.add_argument("-M", "--method",   type=int, default=0, help='\nTool method\nvalues 0-3\nMike' )
+    parser.add_argument("-O", "--operation",type=int, default=0, help='\noperation mode\nvalues 0,1\n0=add (default)\n1=delete' )
+ 
+
+    args = parser.parse_args()
+
+    if len(args.pos_args) == 0:
+        printf("Invalid number of positional arguments",1)
+        parser.print_help()
+        exit(0)
+
+
+    g_control_data["nucleus"]           = args.pos_args[0]
+    g_control_data["nucleus_user"]      = args.user_id
+    g_control_data["nucleus_password"]  = args.password
+
+    startupOmniverse()
+    
+    #   check to see if we are and Admin. This is dumb, must be a better way to figure this out.
+    (res,_) = omni.client.remove_group(get_nucleus_url(),"dummyMiriceXYZ")
+    if res.name == "ERROR_ACCESS_DENIED":
+        printf("Must be an Admin Nucleus user to use this tool",1)
+        exit(-1)
+
+
+
+    # method root folder operations
+    # use this method to create a root project folder. This will house
+    # many sub-projects
+
+    if g_methods[args.method] == 'Root Project':
+        project_root_url = get_nucleus_url()+"/"+args.pos_args[1]
+        res = None
+        
+        if args.operation == 0:
+            res = omni.client.create_folder(project_root_url)
+            if res.name != "OK":
+                printf(f"Failure to create root level folder at '{project_root_url}' {res.name}",1)
+                exit(-1)    
+        else: 
+            res = omni.client.delete_single(project_root_url)
+
+        printf(f"Operation {g_methods[args.method]}:{g_operation[args.operation]}",0)
+
+    if g_methods[args.method] == 'Sub Project':
+        sub_project_folder_url = get_nucleus_url()+"/"+args.pos_args[1]+"/"+args.pos_args[2]
+        sub_project_group_user = args.pos_args[1]+"_"+args.pos_args[2]+"_Users"
+        
+        if args.operation == 0:
+
+            res = omni.client.create_folder(sub_project_folder_url)
+            if res.name != "OK":
+                printf(f"Failure to create project folder {sub_project_folder_url} {res.name}",1)
+                exit(-1)     
+
+            res = omni.client.create_group(get_nucleus_url(), sub_project_group_user)
+            if res.name != "OK":
+                printf(f"Failure to create user group {sub_project_group_user} {res.name}",1)
+                res = omni.client.delete_single(sub_project_folder_url)
+                exit(-1)     
+
+
+            (res,new_acls) = omni.client.get_acls(sub_project_folder_url)
+            
+            new_acls.extend(
+                [omni.client.AclEntry("users",0),
+                 omni.client.AclEntry(sub_project_group_user,omni.client.AccessFlags.READ | omni.client.AccessFlags.WRITE)]
+            )
+            res = omni.client.set_acls(sub_project_folder_url,new_acls)
+            if res.name != "OK":
+                printf(f"Failure to set ACLs for {sub_project_group_user} {res.name}",1)
+                res = omni.client.delete_single(sub_project_folder_url)
+
+        else:
+            res = omni.client.remove_group(get_nucleus_url(), sub_project_group_user)
+            if res.name != "OK":
+                printf(f"Cannot remove group '{sub_project_group_user}' Error = {res.name}",1)
+                exit(0)
+                
+            res = omni.client.delete_single(sub_project_folder_url)
+            if res.name != "OK":
+                printf(f"Cannot delete sub-project '{sub_project_folder_url}' Error = {res.name}",1)
+                exit(0)
+
+        printf(f"Operation {g_methods[args.method]}:{g_operation[args.operation]}",0)
+
+
+        
+
+    # method manage user in a Project_root/Project. 
+    # either add or remove. Note: existing_user must already be
+    # present in nucleus
+
+    if g_methods[args.method] == 'User':
+        existing_user = args.pos_args[3]
+        sub_project_group = args.pos_args[1]+"_"+args.pos_args[2]+"_Users"
+
+        (res,list) = omni.client.get_users(get_nucleus_url())
+        if res.name != "OK":
+            printf("cannot get existing user list",1)
+            exit(0)
+    
+        if existing_user not in list:
+            printf(f"No existing user named '{existing_user}'",1)
+            exit(0)
+
+        if args.operation == 0:
+            res = omni.client.add_user_to_group(
+                get_nucleus_url(),
+                existing_user,
+                sub_project_group
+            )
+        else:
+            res = omni.client.remove_user_from_group(
+                get_nucleus_url(),
+                existing_user,
+                sub_project_group
+            )
+        if res.name != "OK":
+            printf(f"Failed to {g_operation[args.operation]} '{existing_user}' from group '{sub_project_group}' \n error={res.name}")
+            exit(0)
+                  
+        printf(f"Operation {g_methods[args.method]}:{g_operation[args.operation]}",0)
+
+
+
+    shutdownOmniverse()
+    
