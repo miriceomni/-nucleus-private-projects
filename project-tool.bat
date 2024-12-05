6fd4e033535a675754540c921c29af22622b765a
Michael Rice <Mike>
2024-12-05 00:53:15 +0000

    udpate paths

--- project-tool.bat
+++ project-tool.bat
@@ -8,10 +8,10 @@ pushd "%~dp0"
 ::  Change this line to point to the downloaded (and built)
 ::  Client library root dir. 
 ::
-set CLIENT_LIB_SDK_DIR=C:\omniverse\nucleus-private-projects\connect-sdk\connect-samples-205.0.0\
+set CLIENT_LIB_SDK_DIR=C:\omniverse\nucleus-private-projects\connect-sdk\connect-samples-205.0.0
 
-set USD_LIB_DIR=%CLIENT_LIB_SDK_DIR%_build\windows-x86_64\release
-set PYTHON=%CLIENT_LIB_SDK_DIR%_build\target-deps\python\python.exe
+set USD_LIB_DIR=%CLIENT_LIB_SDK_DIR%\_build\windows-x86_64\release
+set PYTHON=%CLIENT_LIB_SDK_DIR%\_build\target-deps\python\python.exe
 
 set PATH=%PATH%;%USD_LIB_DIR%
 set PYTHONPATH=%USD_LIB_DIR%\python;%USD_LIB_DIR%\bindings-python
